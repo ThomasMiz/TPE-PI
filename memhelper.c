0068e4595f5bac8e32da5af3f4e37445a5f4baf9
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2020-12-03 12:50:09 -0300

    Added memory helper auxilary functions

---
+++ memhelper.c
@@ -0,0 +1,35 @@
+#include "memhelper.h"
+#include <ctype.h>
+
+int tryMalloc(void **ptr, size_t size)
+{
+    void *newPtr = malloc(size);
+    if (newPtr == NULL)
+        return 0;
+
+    *ptr = newPtr;
+    return 1;
+}
+
+int tryRealloc(void **ptr, size_t newSize)
+{
+    void *newPtr = realloc(*ptr, newSize);
+    if (newPtr == NULL)
+    {
+        free(*ptr);
+        return 0;
+    }
+
+    *ptr = newPtr;
+    return 1;
+}
+
+int tryCalloc(void **ptr, size_t nmemb, size_t size)
+{
+    void *newPtr = calloc(nmemb, size);
+    if (newPtr == NULL)
+        return 0;
+
+    *ptr = newPtr;
+    return 1;
+}
---
+++ memhelper.h
@@ -0,0 +1,20 @@
+// Provides some basic "wrappers" around memory management functions
+// to help with allocating memory.
+
+#include <stdlib.h>
+
+// Attempts to malloc a given amount of continuos bytes in memory.
+// If the operation is successful, a pointer to the allocated memory is
+// left where indicated by the "ptr" parameter and the function returns 1.
+// If the operation fails, ptr is unmodified and the function returns 0.
+int tryMalloc(void **ptr, size_t size);
+
+// Attempts to realloc the given pointer. If the operation is successful,
+// the reallocated pointer is left in ptr and the function returns 1.
+// If the realloc fails, the original ptr IS FREED and 0 is returned.
+int tryRealloc(void **ptr, size_t newSize);
+
+// Attempts to calloc the given amount of memory and saves the allocated,
+// zeroed-out memory to the location indicated by the "ptr" parameter.
+// Returns 1 if successful, or 0 if the operation failed (and ptr is unmodified).
+int tryCalloc(void **ptr, size_t nmemb, size_t size);
