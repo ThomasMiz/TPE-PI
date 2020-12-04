e1850ea7c03463f6f4b47bb05b5b5cbf03801c2e
ThomasMiz <32400648+ThomasMiz@users.noreply.github.com>
2020-12-04 19:19:56 -0300

    Added comments to specieTrieADT

--- specieTrieADT.c
+++ specieTrieADT.c
@@ -3,18 +3,31 @@
 #include "specieTrieADT.h"
 #include "memhelper.h"
 
-#include <stdio.h>
+// Un-comment this if you want to enable speciePrintAll().
+// #include <stdio.h>
 
+/* The amount of letters in the english alphabet. */
 #define LETTER_COUNT ('z' - 'a' + 1)
+
+/* The amount of child nodes each node in the tree has. */
 #define NODE_COUNT (LETTER_COUNT + 1)
 
+/* The lowest depth value for a node. Used for calculations. */
+#define LOWEST_NODE_DEPTH 0
+
+/* Represents a single node in the tree. */
 typedef struct trieNode
 {
+    /* How many times a string that leads to this node has been added to the tree. */
     unsigned int count;
 
+    /* The node that leads to this node, or NULL if this is the first node. */
     struct trieNode *previous;
+    /* The character index in the previous node, in which this node is located. */
     int previousCharIndex;
 
+    /* This node's child nodes, ordered alphabetically.
+    ** When a string's char isn't a letter, the last index in this array is used. */
     struct trieNode *nextNodes[NODE_COUNT];
 } TNode;
 
@@ -23,6 +36,7 @@ struct specieTrieCDT
     TNode trie;
 };
 
+/* Gets the index in the TNode.nextNodes array in which a char belongs. */
 static int getCharIndex(char c)
 {
     int a = tolower(c) - 'a';
@@ -31,6 +45,7 @@ static int getCharIndex(char c)
     return a;
 }
 
+/* Gets the character that leads to the specified index in the TNode.nextNodes array. */
 static char getIndexChar(int index)
 {
     if (index == LETTER_COUNT)
@@ -47,6 +62,7 @@ specieTrieADT newSpecieTrie()
     return s;
 }
 
+/* Recursively goes through all the nodes and subnodes, freeing all of them. */
 static void freeTrieRecursive(TNode *node)
 {
     for (int i = 0; i < NODE_COUNT; i++)
@@ -58,6 +74,9 @@ static void freeTrieRecursive(TNode *node)
 
 void freeSpecieTrie(specieTrieADT trie)
 {
+    // Since the first node is contained inside the specieTrieCDT, we can't
+    // free() it (it gets freed when we free the struct specieTrieCDT).
+    // So we recursively free all of it's children, then free the CDT.
     for (int i = 0; i < NODE_COUNT; i++)
         if (trie->trie.nextNodes[i] != NULL)
             freeTrieRecursive(trie->trie.nextNodes[i]);
@@ -65,6 +84,9 @@ void freeSpecieTrie(specieTrieADT trie)
     free(trie);
 }
 
+/* Creates a single node and adds it to the tree, setting is "previous" field
+** and placing the new node in the correct index in the previous node's nextNode.
+** If memory can't be allocated, returns NULL. Otherwise, returns the new node. */
 static TNode *addNode(TNode *prev, int charIndex)
 {
     TNode *newNode;
@@ -80,30 +102,40 @@ static TNode *addNode(TNode *prev, int charIndex)
 
 enum TRIE_ERR specieAddOne(specieTrieADT trie, const char *specieName)
 {
+    // We first check that we received a non-null-or-empty string.
     if (specieName == NULL || specieName[0] == '\0')
         return TRIE_BAD_NAME;
 
+    // We initialize the prev and current variables as the root node and NULL respectively.
     TNode *prev = &(trie->trie);
     TNode *current = NULL;
 
+    // We loop through specieName's characters, in each step going down the correct branch
+    // in the tree (creating nodes as necessary) until we've reached the end of the string.
     while (specieName[0] != '\0')
     {
         int currentCharIndex = getCharIndex(specieName[0]);
 
         current = prev->nextNodes[currentCharIndex];
 
+        // If the node 'current' doesnt exist, we create it. If that fails, we return TRIE_NO_MEMORY.
         if (current == NULL && (current = addNode(prev, currentCharIndex)) == NULL)
             return TRIE_NO_MEMORY;
 
         specieName++;
-
         prev = current;
     }
 
+    // We increment current->count, to indicate one more ocurrance of this string.
+    // At this point, we're ensured current is not NULL, because since the string
+    // isn't null nor empty, we must have entered the while loop at least once.
     current->count++;
     return TRIE_OK;
 }
 
+/* Recursively goes through all the nodes and subnodes, comparing and leaving
+** the node with the highest node.count in the variable pointed to by mostPopular
+** and said node's depth in the variable pointer to by mostDepth. */
 static void findMostPopularRecursive(TNode *currentNode, size_t currentDepth, TNode **mostPopular, size_t *mostDepth)
 {
     if (currentNode == NULL)
@@ -124,7 +156,9 @@ char *specieMostPopular(specieTrieADT trie)
     TNode *mostPopular = NULL;
     size_t mostDepth = 0;
 
-    findMostPopularRecursive(&(trie->trie), 0, &mostPopular, &mostDepth);
+    // We recursively search for the node with the highest node.count.
+    findMostPopularRecursive(&(trie->trie), LOWEST_NODE_DEPTH, &mostPopular, &mostDepth);
+    mostDepth -= LOWEST_NODE_DEPTH;
 
     if (mostPopular == NULL || mostPopular->count == 0)
         return NULL;
@@ -133,6 +167,8 @@ char *specieMostPopular(specieTrieADT trie)
     if (!tryMalloc((void **)&str, mostDepth + 1))
         return NULL;
 
+    // We backtrack from mostPopular all the way back to the tree's root, filling
+    // up the string along the way.
     TNode *node = mostPopular;
     for (int i = 0; node->previousCharIndex >= 0; i++)
     {
@@ -140,7 +176,10 @@ char *specieMostPopular(specieTrieADT trie)
         node = node->previous;
     }
 
+    // Since the trie doesn't keep track of casing, we uppercase the first character.
     str[0] = toupper(str[0]);
+
+    // We can also uppercase all characters after a ' '.
     /*for (int i = 1; i < mostDepth; i++)
         if (str[i - 1] == ' ')
             str[i] = toupper(str[i]);*/
--- specieTrieADT.h
+++ specieTrieADT.h
@@ -1,7 +1,7 @@
 #ifndef specieTrieADT_h
 #define specieTrieADT_h
 
-// Defines errors that can be thrown by operations from specieTrieADT.
+/* Defines errors that can be thrown by operations from specieTrieADT. */
 enum TRIE_ERR
 {
     TRIE_OK = 0,
@@ -11,18 +11,20 @@ enum TRIE_ERR
 
 typedef struct specieTrieCDT *specieTrieADT;
 
-// Created a new empty specieTrieADT.
+/* Created a new empty specieTrieADT. If memory can't be allocated, returns NULL. */
 specieTrieADT newSpecieTrie();
 
-// Frees all the resources used by a specieTrieADT.
+/* Frees all the resources used by a specieTrieADT. */
 void freeSpecieTrie(specieTrieADT);
 
-// Increments one to the counter of a specific specie.
+/* Increments one to the counter of a specific specie. Returns the error code of the opration. */
 enum TRIE_ERR specieAddOne(specieTrieADT, const char *specieName);
 
-// Finds the most popular specie in the trie and returns a new string with it's name.
+/* Finds the most popular specie in the trie and returns a new string with it's name.
+** Returns NULL if the trie is empty or if memory couldn't be allocated. */
 char *specieMostPopular(specieTrieADT);
 
+/* Prints all the elements in the trie, indicating how many ocurrences each one has. */
 //void speciePrintAll(specieTrieADT);
 
 #endif
